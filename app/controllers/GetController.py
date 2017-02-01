234cac115920eb351bbe80cd607624e5452ab880
LiamWr <liammail>
2017-02-01 16:05:14 +0100

    added stuff

--- app/controllers/GetController.py
+++ app/controllers/GetController.py
@@ -1,49 +1,17 @@
 from django.shortcuts import HttpResponse
 from bson.json_util import dumps
 from app.services.conn import *
-from bson import ObjectId
 #Defining services
 from app.services import GetService, ValidateService
 
 def Get(request, catch_all):
-    #Terms allowed to be used in URL
-    allowed_terms = [
-        'col', 
-        'name', 
-        'id', 
-        'public', 
-        'tags', 
-        'views', 
-        'trending', 
-        'media', 
-        'date', 
-        'alias', 
-        'weekly', 
-        'nodes',
-        'email'
-    ]
-
-    #allowed_col_value = ['user','node','tag']
-
-    attributes = {}
-
     #Catches URL and converts items and values into a dict
     catch_all = dict(item.split("=") for item in catch_all.split("&"))
-
-    #1. Checks if items in URL are legal
-    #2. If id exist change key and value to proper format for mongoDB use
-    #3. if col exist put col value in new var
-    for term in allowed_terms:
-        for catch_item in catch_all:
-            if catch_item == term:
-                if catch_item == 'id':
-                    attributes['_id'] = ObjectId(catch_all.get(catch_item))
-                elif catch_item == 'col':
-                    collection = catch_all.get(catch_item)
-                else:
-                    attributes[catch_item] = catch_all.get(catch_item)
     
-    #Note: Send dict instead
-    result = GetService.GetRequest(collection, attributes)
+    #Validate for mongodb query use
+    dict1 = ValidateService.ValidateGetTerms(catch_all)
 
+    #Makes a get request
+    result = GetService.GetRequest(dict1)
+    
     return HttpResponse(dumps(result))
--- app/services/ValidateService.py
+++ app/services/ValidateService.py
@@ -1,5 +1,50 @@
-def ValidateGetTerms(dict):
-    print(dict)
+from bson import ObjectId
 
-    result = dict
+#Validates a dictionary for mongodb query use
+#Recieves dictionary in a normal format
+#Returns a dictionary with dictionaries inside
+def ValidateGetTerms(dict1):
+    #Defining variabless
+    attribute = {}
+    result = {}
+    
+    #Allowed terms with type to be used when calling database
+    allowed_terms = {
+        'col': str, 
+        'name': str, 
+        'id': str, 
+        'public': int, 
+        'tags': str, 
+        'views': int, 
+        'trending': int, 
+        'media': int, 
+        'date': int, 
+        'alias': str, 
+        'weekly': int, 
+        'nodes': str,
+        'select': str,
+        'email': str
+    }
+
+    #1. Checks if items in URL are legal
+    for term in allowed_terms:
+        for dict1_item in dict1:
+            if dict1_item == term:
+                if allowed_terms.get(term) == int:
+                    attribute[dict1_item] = int(dict1.get(dict1_item))
+                    result['attribute'] = attribute
+                else:
+                    #Converts id key and value to proper attributes for mongodb use
+                    if dict1_item == 'id':
+                        attribute['_id'] = ObjectId(dict1.get(dict1_item))
+                        result['attribute'] = attribute
+                    #Puts
+                    elif dict1_item == 'col':
+                        result['collection'] = [dict1.get(dict1_item)]
+                    elif dict1_item == 'select':
+                        result['select'] = [dict1.get(dict1_item)]
+                    else:
+                        attribute[dict1_item] = dict1.get(dict1_item)
+                        result['attribute'] = attribute
+
     return result
