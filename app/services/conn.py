140ab6cbf3fd38e3b734282cb57926e2042a6b03
Kevin Tatou <kevintatou94@gmail.com>
2017-04-04 14:03:33 +0200

    added conn

--- app/services/conn.py
+++ app/services/conn.py
@@ -7,16 +7,16 @@ client = MongoClient('localhost', 4321)
 db = client['idela']
 
 #Defining collections in idela database
-#user = db.user
-#node = db.node
-#tag = db.tag
-#feedback = db.feedback
-#softdelete = db.softdelete
+user = db.user
+node = db.node
+tag = db.tag
+feedback = db.feedback
+softdelete = db.softdelete
 
-user = db.user_test
-node = db.node_test
-tag = db.tag_test
-feedback = db.feedback_test
+#user = db.user_test
+#node = db.node_test
+#tag = db.tag_test
+#feedback = db.feedback_test
 softdelete = db.softdelete_test
 
 #Jespers Kandidat
