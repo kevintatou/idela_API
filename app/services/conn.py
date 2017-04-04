946dc3dcddb042727dfd2c0f14a1ee88c8b09b04
Kevin Tatou <kevintatou94@gmail.com>
2017-04-04 14:00:56 +0200

    conn.py added

---
+++ app/services/conn.py
@@ -0,0 +1,23 @@
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 4321)
+#print(client.test.command('buildinfo'))
+
+#Get idela database
+db = client['idela']
+
+#Defining collections in idela database
+#user = db.user
+#node = db.node
+#tag = db.tag
+#feedback = db.feedback
+#softdelete = db.softdelete
+
+user = db.user_test
+node = db.node_test
+tag = db.tag_test
+feedback = db.feedback_test
+softdelete = db.softdelete_test
+
+#Jespers Kandidat
+placeholder = db.placeholder
